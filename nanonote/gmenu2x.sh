db48e7387d7d17718585105030447d0841a637a7
kyak <bas@bmail.ru>
2011-02-12 23:04:22 +0300

    gmenu2x: load unicode mapping file at start
    needed for unicode box drawing characters

--- nanonote/gmenu2x.sh
+++ nanonote/gmenu2x.sh
@@ -2,7 +2,8 @@
 
 source /etc/profile
 
-setfont2  /usr/share/setfont2/un-fuzzy-6x10-font.pnm 
+setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
+loadunimap /usr/share/setfont2/ben_uni.trans
 clear
 
 cd /usr/share/gmenu2x
