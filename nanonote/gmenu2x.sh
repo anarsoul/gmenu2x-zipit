e0831be4a3d9b22a79e63cb0b83147819603d86c
Xiangfu Liu <xiangfu@sharism.cc>
2011-05-27 21:41:12 +0800

    add color ash shell, add the method to gmenu2x.sh as comment

--- nanonote/gmenu2x.sh
+++ nanonote/gmenu2x.sh
@@ -4,7 +4,8 @@ source /etc/profile
 
 setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
 loadunimap /usr/share/setfont2/ben_uni.trans
-clear
 
-cd /usr/share/gmenu2x
-./gmenu2x 
+#setfont  /usr/share/kbd/consolefonts/kernel-6x11-font
+
+clear
+cd /usr/share/gmenu2x && ./gmenu2x
