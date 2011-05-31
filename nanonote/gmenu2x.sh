b068aa04d4cf7f7e0e6f68f4abe048a3b6c719ce
Xiangfu Liu <xiangfu@sharism.cc>
2011-05-31 09:01:16 +0800

    nanonote: using color font

--- nanonote/gmenu2x.sh
+++ nanonote/gmenu2x.sh
@@ -2,10 +2,10 @@
 
 source /etc/profile
 
-setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
-loadunimap /usr/share/setfont2/ben_uni.trans
+#setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
+#loadunimap /usr/share/setfont2/ben_uni.trans
 
-#setfont  /usr/share/kbd/consolefonts/kernel-6x11-font
+setfont  /usr/share/kbd/consolefonts/kernel-6x11-font
 
 clear
 cd /usr/share/gmenu2x && ./gmenu2x
