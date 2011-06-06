4b22318b09ed70e84556783cae4df9987acbdfb8
kyak <bas@bmail.ru>
2011-06-06 10:05:48 +0400

    Update gmenu2x.sh for install_locations changes

--- data/platform/nanonote/gmenu2x.sh
+++ data/platform/nanonote/gmenu2x.sh
@@ -5,7 +5,7 @@ source /etc/profile
 #setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
 #loadunimap /usr/share/setfont2/ben_uni.trans
 
-setfont  /usr/share/kbd/consolefonts/kernel-6x11-font
+setfont /usr/share/kbd/consolefonts/kernel-6x11-font
 
 clear
-cd /usr/share/gmenu2x && ./gmenu2x
+/usr/bin/gmenu2x.bin
