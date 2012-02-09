c913ceab59882d5d8c8a1e6225c5373e43f31368
Vasily Khoruzhick <anarsoul@gmail.com>
2012-02-09 22:50:05 +0300

    Zipit Z2: update gmenu2x.sh and input.conf
    No need to load font in startup script,
    use prev and next keys for switching between tabs
    
    Signed-off-by: Vasily Khoruzhick <anarsoul@gmail.com>

--- data/platform/zipitz2/gmenu2x.sh
+++ data/platform/zipitz2/gmenu2x.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 source /etc/profile
-
-#setfont2 /usr/share/setfont2/un-fuzzy-6x10-font.pnm
-#loadunimap /usr/share/setfont2/ben_uni.trans
-
-/usr/sbin/loadfont < /etc/console-setup/Uni2-Terminus12x6.psf
-
 trap "" hup
-
 clear
 exec /usr/bin/gmenu2x.bin
